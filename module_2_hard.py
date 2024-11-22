639573e63680f94216efd70d63e8e4be9a90aa6e
anton13samarin <big13sav@gmail.com>
2024-11-22 13:36:54 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+def generator_parolia(chislo):
+    parol_chisla = ('')
+    for i in range (1, chislo):
+        for j in range (2, chislo):
+            if j <= i:
+                continue
+            if chislo % (i + j) == 0:
+                parol_chisla += str(i) + str(j)
+    return parol_chisla
+for i in range(3, 21):
+    parol = generator_parolia(i)
+    print(i, parol, sep=" - ")
