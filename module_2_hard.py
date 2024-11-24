3d1ebdca4e950cdfb82dd00d90d9b616a35f1a50
anton13samarin <big13sav@gmail.com>
2024-11-24 12:04:08 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,34 @@
-def generator_parolia(chislo):
-    parol_chisla = ('')
-    for i in range (1, chislo):
-        for j in range (2, chislo):
-            if j <= i:
+def x_password(number):
+    password = ''
+    a = 1
+    while a < number:
+        b = 2
+        while b < number:
+            if b <= a:
+                b += 1
                 continue
-            if chislo % (i + j) == 0:
-                parol_chisla += str(i) + str(j)
-    return parol_chisla
+            if number % (a + b) == 0:
+                password += str(a) + str(b)
+            b += 1
+        a += 1
+    return password
+
+i = 3
+while i < 21:
+    result = x_password(i)
+    print(f"{i} - {result}")
+    i += 1
+
+
+def x_password(number):
+    password = ''
+    for a in range(1, number):
+        for b in range(2, number):
+            if b <= a:
+                continue
+            if number % (a + b) == 0:
+                password += str(a) + str(b)
+    return password
 for i in range(3, 21):
-    result = generator_parolia(i)
-    print(i, result, sep=" - ")
+    result = x_password(i)
+    print(f"{i} - {result}")
